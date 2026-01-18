66fc5e339b6873fe9b72f6575dbf20ec45f0d3ca
oDarki <kevinmalta018@gmail.com>
2026-01-17 22:20:33 -0300

    Update main.py
    fsd

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
-from telegram import Bot
 import asyncio
+from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
 bot = Bot(token=BOT_TOKEN)
 
-async def teste():
-    updates = await bot.get_updates()
-    print(updates)
+async def main():
+    while True:
+        try:
+            await bot.send_message(
+                chat_id=CHANNEL_ID,
+                text="🤖 Bot Amazon está online e funcionando!"
+            )
+            await asyncio.sleep(3600)  # 1 hora
+        except Exception as e:
+            print("Erro:", e)
+            await asyncio.sleep(10)
 
-asyncio.run(teste())
+asyncio.run(main())
