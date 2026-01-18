170f6d4e99ec5954862727d9ba2673a9c0c565ab
oDarki <kevinmalta018@gmail.com>
2026-01-17 21:43:03 -0300

    Update main.py
    sada

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-import asyncio
 import os
 from telegram import Bot
+import asyncio
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-AMAZON_AFILIADO = os.getenv("AMAZON_AFILIADO")
-
 bot = Bot(token=BOT_TOKEN)
 
-async def postar():
-    await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=(
-            "🔥 OFERTA AMAZON 🔥\n\n"
-            "📦 Produto em promoção\n"
-            "💰 Preço especial\n\n"
-            f"👉 https://www.amazon.com.br/?tag={AMAZON_AFILIADO}"
-        )
-    )
+async def teste():
+    updates = await bot.get_updates()
+    print(updates)
 
-async def loop():
-    while True:
-        try:
-            await postar()
-            await asyncio.sleep(3600)  # 1 hora
-        except Exception as e:
-            print("Erro:", e)
-            await asyncio.sleep(60)
-
-asyncio.run(loop())
-
+asyncio.run(teste())
