c385a88f6bef9023257d6d80570383ec63b81ae6
oDarki <kevinmalta018@gmail.com>
2026-01-17 22:25:25 -0300

    Update main.py
    updt

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import os
 import asyncio
 from telegram import Bot
+from flask import Flask
+import threading
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 bot = Bot(token=BOT_TOKEN)
+app = Flask(__name__)
 
-async def main():
+@app.route("/")
+def home():
+    return "Bot Amazon rodando"
+
+async def bot_loop():
     while True:
         try:
             await bot.send_message(
                 chat_id=CHANNEL_ID,
-                text="🤖 Bot Amazon está online e funcionando!"
+                text="🤖 Bot Amazon está online!"
             )
-            await asyncio.sleep(3600)  # 1 hora
+            await asyncio.sleep(60)
         except Exception as e:
             print("Erro:", e)
             await asyncio.sleep(10)
 
-asyncio.run(main())
+def start_bot():
+    asyncio.run(bot_loop())
+
+if __name__ == "__main__":
+    threading.Thread(target=start_bot).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
